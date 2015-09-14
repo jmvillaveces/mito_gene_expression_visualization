c482c2f19c1c3536440439baf9ace1aca4e284f7
jVillaveces <secevalliv@gmail.com>
2015-09-14 13:47:37 +0200

    save svg function

--- js/views/buttonGroup.js
+++ js/views/buttonGroup.js
@@ -3,7 +3,8 @@ var templates = require('../templates');
 module.exports = Backbone.View.extend({
     
     events: {
-        'change input[name=vis_setting]' : 'onVisSettingChange'
+        'change input[name=vis_setting]' : 'onVisSettingChange',
+        'click #save' : 'onSaveClick'
     },
     
     template: templates.buttonGroup,
@@ -22,5 +23,11 @@ module.exports = Backbone.View.extend({
         }else{
             App.views.vis.displayTowardProcess();
         }
+    },
+    
+    onSaveClick : function(e){
+        d3.selectAll("#save")
+            .attr("href", "data:image/svg+xml;charset=utf-8;base64," + btoa(unescape(encodeURIComponent(d3.selectAll("#svg_vis")
+                .attr("version", "1.1").attr("xmlns", "http://www.w3.org/2000/svg").node().parentNode.innerHTML)))).attr("download",'image.svg');
     }
 });
