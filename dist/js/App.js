334cbcf82bc0737728a834c01e30c124de3d9ad6
jVillaveces <secevalliv@gmail.com>
2016-04-20 15:46:14 +0200

    recompiled

--- dist/js/App.js
+++ dist/js/App.js
@@ -10,6 +10,17 @@ circle.getRadius = function(area){
 circle.getArea = function(radius){
     return radius * radius * Math.PI;
 };
+
+circle.areaScale = function(domain, range){
+
+    var areaRange = _.map(range, circle.getArea),
+        scale = d3.scale.log().domain(domain).range(areaRange);
+    
+    return function(val){
+        return circle.getRadius(scale(val));
+    };
+};
+
 module.exports = circle;
 },{}],2:[function(require,module,exports){
 module.exports={
@@ -402,8 +413,6 @@ var _onMouseOverNode = function(node){
                 s_opacity = d3.select('#' + source.id).attr('opacity'),
                 t_opacity = d3.select('#' + target.id).attr('opacity');
             
-            console.log(source.id, target.id);
-            console.log(s_opacity, t_opacity);
             if(s_opacity > 0.1 && t_opacity > 0.1){
             
                 d3.select(this)
@@ -425,6 +434,7 @@ var _onMouseOverNode = function(node){
     }
         
     _processCircles.transition(3000).attr('opacity', changeOpacity);
+    //_geneCircles.transition(3000).attr('opacity', changeOpacity);
     _processAnnotations.transition(2800).style('opacity', changeOpacity);
 };
 
@@ -505,7 +515,7 @@ var _formatData = function(json){
         return Math.abs(d.Log2fold_change);
     });
     
-    _radiusScale = d3.scale.log().domain([1, max_abs_log2 + 1 ]).range([2, 30]);
+    _radiusScale = _areaCalc.areaScale([1, max_abs_log2 + 1 ], [2, 25]);
     _.each(all_nodes, function(d){
         d.radius = _radiusScale(Math.abs(d.Log2fold_change) + 1);
     });
@@ -673,6 +683,9 @@ var _createVis = function(){
     
     _vis = d3.select(_selector).append('svg').attr('class', 'canvas').attr('width', _width).attr('id', 'svg_vis');
     
+    // Render links before circles
+    _initLinks();
+    
     _geneCircles = _vis.selectAll('circle').data(_data.nodes, function(d){ return d.id; });
     
     _geneCircles.enter().append('circle')
@@ -697,7 +710,6 @@ var _createVis = function(){
         .call(_tip);
     
     _initChart();
-    _initLinks();
     _initProcessAnnotations();
     _initProcesses();
     _createLegend();
