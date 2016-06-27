d700cad11ec9aa118854b1eb8627a3095aedc9de
jmvillaveces <secevalliv@gmail.com>
2016-06-27 17:54:45 +0200

    processes with pack layout

--- js/templates.js
+++ js/templates.js
@@ -7,27 +7,27 @@ this["Templates"] = this["Templates"] || {};
 this["Templates"]["annotation"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return "<div class=\"node theme\" style=\"vertical-align: middle;display: inline-block;\">\n    <span class=\"glyphicon glyphicon-plus-sign\" style=\"position: absolute;margin-left:-65%;opacity:0;\"></span>\n    <div>"
+  return "<div class=\"node theme\" style=\"vertical-align: middle;display: inline-block;\">\r\n    <span class=\"glyphicon glyphicon-plus-sign\" style=\"position: absolute;margin-left:-65%;opacity:0;\"></span>\r\n    <div>"
     + container.escapeExpression(((helper = (helper = helpers.Process || (depth0 != null ? depth0.Process : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"Process","hash":{},"data":data}) : helper)))
-    + "</div>\n</div>";
+    + "</div>\r\n</div>";
 },"useData":true});
 
 this["Templates"]["buttonGroup"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"form-group btn-group\" data-toggle=\"buttons\">\n    <label class=\"btn btn-default active\" data-toggle=\"tooltip\" data-placement=\"bottom\">\n        <input type=\"radio\" name=\"vis_setting\" value=\"network\" autocomplete=\"off\" checked=\"\"> Network\n    </label>\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\n        <input type=\"radio\" name=\"vis_setting\" value=\"all\" autocomplete=\"off\"> Gene Expression\n    </label>\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\n        <input type=\"radio\" name=\"vis_setting\" value=\"process\" autocomplete=\"off\"> Gene Expression by Process\n    </label>\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\n        <input type=\"radio\" name=\"vis_setting\" value=\"chart\" autocomplete=\"off\"> Gene Expression Chart\n    </label>\n</div>\n<ul class=\"nav nav-pills pull-right\">\n    <li role=\"presentation\"><input type=\"text\" class=\"form-control\" placeholder=\"Search gene by name...\"></li>\n</ul>\n\n";
+    return "<div class=\"form-group btn-group\" data-toggle=\"buttons\">\r\n    <label class=\"btn btn-default active\" data-toggle=\"tooltip\" data-placement=\"bottom\">\r\n        <input type=\"radio\" name=\"vis_setting\" value=\"network\" autocomplete=\"off\" checked=\"\"> Network\r\n    </label>\r\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\r\n        <input type=\"radio\" name=\"vis_setting\" value=\"all\" autocomplete=\"off\"> Gene Expression\r\n    </label>\r\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\r\n        <input type=\"radio\" name=\"vis_setting\" value=\"process\" autocomplete=\"off\"> Gene Expression by Process\r\n    </label>\r\n    <label class=\"btn btn-default\" data-toggle=\"tooltip\" data-placement=\"bottom\">\r\n        <input type=\"radio\" name=\"vis_setting\" value=\"chart\" autocomplete=\"off\"> Gene Expression Chart\r\n    </label>\r\n</div>\r\n<ul class=\"nav nav-pills pull-right\">\r\n    <li role=\"presentation\"><input type=\"text\" class=\"form-control\" placeholder=\"Search gene by name...\"></li>\r\n</ul>\r\n\r\n";
 },"useData":true});
 
 this["Templates"]["main"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<!-- Page Content -->\n<div class=\"container\">\n\n    <div id=\"navbar\" class=\"nav_bar\"></div>\n        \n    <div class=\"legend\">\n        <div class=\"row\">\n            <div class=\"col-md-4\">\n                <div class=\"col-md-6\"><strong>Color</strong> shows gene regulation</div>\n                <div id=\"color_scale\" class=\"col-md-6\"></div>\n            </div>\n            <div class=\"col-md-4\">\n                <div class=\"col-md-7\"><strong>Dark Borders</strong> show mutations</div>\n                <div id=\"border_scale\" class=\"col-md-5\"></div>\n            </div>\n            <div class=\"col-md-4\">\n                <div class=\"col-md-6\"><strong>Size</strong> shows Log2 fold change</div>\n                <div id=\"size_scale\" class=\"col-md-6\"></div>\n            </div>\n        </div>\n    </div>\n        \n    <div class=\"row\">\n        <div class=\"col-md-12\">\n            <div id=\"vis\" class=\"vis\"></div>\n        </div>        \n    </div>\n    \n</div>";
+    return "<!-- Page Content -->\r\n<div class=\"container\">\r\n\r\n    <div id=\"navbar\" class=\"nav_bar\"></div>\r\n        \r\n    <div class=\"legend\">\r\n        <div class=\"row\">\r\n            <div class=\"col-md-4\">\r\n                <div class=\"col-md-6\"><strong>Color</strong> shows gene regulation</div>\r\n                <div id=\"color_scale\" class=\"col-md-6\"></div>\r\n            </div>\r\n            <div class=\"col-md-4\">\r\n                <div class=\"col-md-7\"><strong>Dark Borders</strong> show mutations</div>\r\n                <div id=\"border_scale\" class=\"col-md-5\"></div>\r\n            </div>\r\n            <div class=\"col-md-4\">\r\n                <div class=\"col-md-6\"><strong>Size</strong> shows Log2 fold change</div>\r\n                <div id=\"size_scale\" class=\"col-md-6\"></div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n        \r\n    <div class=\"row\">\r\n        <div class=\"col-md-12\">\r\n            <div id=\"vis\" class=\"vis\"></div>\r\n        </div>        \r\n    </div>\r\n    \r\n</div>";
 },"useData":true});
 
 this["Templates"]["tooltip"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {};
 
-  return "        <div class=\"tip-process\">\n            <strong>Variant Sites:</strong> "
+  return "        <div class=\"tip-process\">\r\n            <strong>Variant Sites:</strong> "
     + container.escapeExpression(((helper = (helper = helpers.Variant_sites || (depth0 != null ? depth0.Variant_sites : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"Variant_sites","hash":{},"data":data}) : helper)))
-    + "\n            <br>\n            "
+    + "\r\n            <br>\r\n            "
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.Chromosome_number : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n        </div>\n";
+    + "\r\n        </div>\r\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var helper;
 
@@ -36,19 +36,19 @@ this["Templates"]["tooltip"] = Handlebars.template({"1":function(container,depth
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div>\n    <div class=\"tip-name\"><strong>"
+  return "<div>\r\n    <div class=\"tip-name\"><strong>"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</strong></div>\n    <div class=\"tip-rule\"></div>\n    <div class=\"tip-process\">"
+    + "</strong></div>\r\n    <div class=\"tip-rule\"></div>\r\n    <div class=\"tip-process\">"
     + alias4(((helper = (helper = helpers.process || (depth0 != null ? depth0.process : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"process","hash":{},"data":data}) : helper)))
-    + "</div>\n    <div class=\"tip-function\">"
+    + "</div>\r\n    <div class=\"tip-function\">"
     + alias4(((helper = (helper = helpers.gene_function || (depth0 != null ? depth0.gene_function : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"gene_function","hash":{},"data":data}) : helper)))
-    + "</div>\n    <div class=\"tip-pvalue\"><strong>Pvalue: "
+    + "</div>\r\n    <div class=\"tip-pvalue\"><strong>Pvalue: "
     + alias4(((helper = (helper = helpers.p_value || (depth0 != null ? depth0.p_value : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"p_value","hash":{},"data":data}) : helper)))
-    + "</strong></div>\n    <div><strong>Log2 fold change:</strong> \n        <span class=\"tip-"
+    + "</strong></div>\r\n    <div><strong>Log2 fold change:</strong> \r\n        <span class=\"tip-"
     + alias4(((helper = (helper = helpers.regulated || (depth0 != null ? depth0.regulated : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"regulated","hash":{},"data":data}) : helper)))
     + "\"><strong>"
     + alias4(((helper = (helper = helpers.Log2fold_change || (depth0 != null ? depth0.Log2fold_change : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Log2fold_change","hash":{},"data":data}) : helper)))
-    + "</strong></span>\n    </div>\n\n"
+    + "</strong></span>\r\n    </div>\r\n\r\n"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.Variant_sites : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "</div>";
 },"useData":true});
--- js/views/process.vis.js
+++ js/views/process.vis.js
@@ -43,7 +43,132 @@ function initVis(){
         .attr('width', width)
         .attr('id', 'svg_vis');
     
-    var pie = d3.layout.pie()
+    var root = { id:'root', genes:data.processes };
+    
+    var diameter = d3.min([width, height]) * 0.8,
+        format = d3.format(",d");
+
+    var pack = d3.layout.pack()
+        .size([diameter - 4, diameter - 4])
+        .children(function(d){ return d.genes;})
+        .value(function(d) { return Math.abs(d.Log2FoldChange); });
+    
+    var nodes = svg.datum(root).selectAll('process')
+                .data(pack.nodes)
+                    .enter().append('g')
+                .attr('class', function(d) { return (d.id === 'root') ? 'root' : d.genes ? 'process' : 'gene'; })
+                .attr('transform', function(d) { return 'translate(' + d.x * 1.3 + ',' + d.y  * 1.3 + ')'; })
+                .attr('id', function(d) { return  d.id; });
+
+    
+    // Append process
+    nodes
+        .filter(function(d) { return d.genes && d.id !== 'root';})
+        .each(handleProcess);
+    
+    nodes
+        .filter(function(d) { return !d.genes;})
+        .each(handleGenes);
+    
+    function handleProcess(d){
+        
+        var g = d3.select(this),
+            innerR = d.r * 0.7,
+            arr = [ 
+                { stroke: stroke('up'), color: fill('up'), Log2FoldChange: d.up.length}, 
+                { stroke: stroke('none'), color: fill('none'), Log2FoldChange: d.none.length }, 
+                { stroke: stroke('down'), color: fill('down'), Log2FoldChange: d.down.length }
+            ],
+            arc = d3.svg.arc()
+                .innerRadius(innerR)
+                .outerRadius(d.r),
+            pie = d3.layout.pie()
+                .sort(null)
+                .value(function(d) { return d.Log2FoldChange; });
+        
+        // Add background circle circle to hide links and listen for events
+        g.selectAll('circle').data([d])
+            .enter().append('circle')
+            .attr('fill', '#ffffff')
+            .attr('id', function(d) { return 'circle_' + d.id; })
+            .attr('r', d.r)
+            .attr('stroke-width', 2)
+            .attr('stroke', function(d){ 
+                var p = _.pluck(d.genes, 'Variant_sites');
+                return (p.join('').length > 0) ? mutationColor : null;
+            });
+        
+        // Create ring
+        g.selectAll('path').data(pie(arr))
+            .enter().append('path')
+            .attr('fill', function(d) { return d.data.color; })
+            .attr('d', arc)
+            .style('pointer-events', 'none'); // only the backgound circle listens to events
+        
+        // Create text path
+        var txtArc = d3.svg.arc()
+            .innerRadius(d.r)
+            .outerRadius(d.r + 11)
+            .startAngle(0)
+            .endAngle(2 * Math.PI);
+        
+        g.append('path')
+            .attr('id', function(d){ return 'txtPath' + d.id; })
+            .attr('d', getPath)
+            .style('fill', 'none');
+        
+        g.append('text')
+                .style("text-anchor","middle")
+            .append('textPath')
+                .attr('xlink:href', function(d){ return '#txtPath' + d.id; })
+	           .attr('startOffset', '50%')	
+                .text(function(d) { return d.Process; });
+    }
+    
+    function handleGenes(d){
+        
+        //console.log(d);
+        
+        var g = d3.select(this);
+        
+        g.selectAll('circle').data(d)
+            .enter().append('circle')
+                .attr('id', function(d) { return  d.id; })
+                .attr('r', function(d){ return d.r; })
+                .attr('fill', function(d){ return fill('up'); })
+                .attr('cx', function(d){ return d.x; })
+                .attr('cy', function(d){ return d.y; })
+                .attr('stroke-width', function(d){ return (d.mutation.length) ? 1.2 : 1; })
+                .attr('stroke', function(d){ return (d.mutation.length) ? mutationColor : stroke('up');})
+                .attr('class', function(d){ return d.parent.id; });
+    }
+    
+    function getPath(d){
+        
+        //M start-x, start-y A radius-x, radius-y, x-axis-rotation, large-arc-flag, sweep-flag, end-x, end-y
+        //M0,300 A200,200 0 0,1 400,300
+        
+        var x1 = - d.r,
+            x2 = d.r,
+            y1 = 0,
+            y2 =  0;
+        
+        return 'M ' + x1 + ',' + y1 + ' A ' + d.r + ',' + d.r +' 0 0, 1, ' + x2 + ',' + y2;
+    }
+    
+      /*node.append("title")
+          .text(function(d) { return d.name + (d.genes ? "" : ": " + format(d.Log2FoldChange)); });
+
+      node.append("circle")
+          .attr("r", function(d) { return d.r; });
+
+      node.filter(function(d) { return !d.genes; }).append("text")
+          .attr("dy", ".3em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.name.substring(0, d.r / 3); });*/
+
+    
+    /*var pie = d3.layout.pie()
         .sort(null)
         .value(function(d) { return d.Log2FoldChange; });
     
@@ -70,19 +195,19 @@ function initVis(){
         .attr('id', function(d) { return  d.id; })
         .attr('class', 'process')
         .attr('opacity', 1)
-        .attr('transform', function(d){ return 'translate(' + d.x + ',' + d.y + ')'; });
+        .attr('transform', function(d){ return 'translate(' + d.x + ',' + d.y + ')'; });*/
     
     
-    processes.each(addElements);
+    //processes.each(addElements);
     
     // Create process Annotations
-    var ann_scale = d3.scale.log().domain(d3.extent(data.processes, function(d){ return d.r; })).range([10,16]);
+    /*var ann_scale = d3.scale.log().domain(d3.extent(data.processes, function(d){ return d.r; })).range([10,16]);
     
     var div = d3.select(selector)
         .append('div')
-        .attr('class', 'node-label-container');
+        .attr('class', 'node-label-container');*/
     
-    processAnnotations = div.selectAll('div').data(data.processes);
+    /*processAnnotations = div.selectAll('div').data(data.processes);
     
     processAnnotations.enter().append('div')
         .html(annTemplate)
@@ -98,7 +223,7 @@ function initVis(){
         })
         .on('click', function(d){
         
-        });
+        });*/
     
     function addElements(p){
         
